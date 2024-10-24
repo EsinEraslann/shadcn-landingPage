6cf3c40b1821b34ff5b9fc9c5c0233d0ebdc8f5d
EsinEraslann <esineraslanx@gmail.com>
2024-10-24 23:06:05 +0300

    template organizated

---
+++ src/components/HeroCards.tsx
@@ -0,0 +1,170 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Badge } from "./ui/badge";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
+import { Check, Linkedin } from "lucide-react";
+import { LightBulbIcon } from "./Icons";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
+export const HeroCards = () => {
+  return (
+    <div className="hidden lg:flex flex-row flex-wrap gap-8 relative w-[700px] h-[500px]">
+      {/* Testimonial */}
+      <Card className="absolute w-[340px] -top-[15px] drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+        <CardHeader className="flex flex-row items-center gap-4 pb-2">
+          <Avatar>
+            <AvatarImage alt="" src="https://github.com/shadcn.png" />
+            <AvatarFallback>SH</AvatarFallback>
+          </Avatar>
+
+          <div className="flex flex-col">
+            <CardTitle className="text-lg">John Doe</CardTitle>
+            <CardDescription>@john_doe</CardDescription>
+          </div>
+        </CardHeader>
+
+        <CardContent>This landing page is awesome!</CardContent>
+      </Card>
+
+      {/* Team */}
+      <Card className="absolute right-[20px] top-4 w-80 flex flex-col justify-center items-center drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+        <CardHeader className="mt-8 flex justify-center items-center pb-2">
+          <img
+            src="/src/assets/myProfile.jpeg"
+            alt="profile"
+            className="absolute grayscale-[0%] -top-12 rounded-full w-24 h-24 aspect-square object-cover"
+          />
+          <CardTitle className="text-center">Esin ERASLAN</CardTitle>
+          <CardDescription className="font-normal text-primary">
+            Frontend Developer
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent className="text-center pb-2">
+          <p>
+            I really enjoy transforming ideas into functional software that
+            exceeds expectations
+          </p>
+        </CardContent>
+
+        <CardFooter>
+          <div>
+            <a
+              rel="noreferrer noo/*  */pener"
+              href="https://www.linkedin.com/in/esineraslan/"
+              target="_blank"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
+            >
+              <span className="sr-only">Linkedin icon</span>
+              <Linkedin size="20" />
+            </a>
+            <a
+              rel="noreferrer noopener"
+              href="https://github.com/EsinEraslann"
+              target="_blank"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
+            >
+              <span className="sr-only">Github icon</span>
+              <GitHubLogoIcon className="w-5 h-5" />
+            </a>
+            <a
+              rel="noreferrer noopener"
+              href="https://codepen.io/EsinEraslann/"
+              target="_blank"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
+            >
+              <span className="sr-only">Codepen icon</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 64 64"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="h-5 w-5"
+                aria-hidden="true"
+              >
+                <path
+                  d="M3.06 41.732L32 60.932l28.94-19.2V22.268L32 3.068l-28.94 19.2zm57.878 0L32 22.268 3.06 41.732m0-19.463L32 41.47l28.94-19.2M32 3.068v19.2m0 19.463v19.2"
+                  stroke-width="5"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </CardFooter>
+      </Card>
+
+      {/* Pricing */}
+      <Card className="absolute top-[150px] left-[50px] w-72  drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+        <CardHeader>
+          <CardTitle className="flex item-center justify-between">
+            Free
+            <Badge variant="secondary" className="text-sm text-primary">
+              Most popular
+            </Badge>
+          </CardTitle>
+          <div>
+            <span className="text-3xl font-bold">$0</span>
+            <span className="text-muted-foreground"> /month</span>
+          </div>
+
+          <CardDescription>
+            Lorem ipsum dolor sit, amet ipsum consectetur adipisicing elit.
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <Button className="w-full">Start Free Trial</Button>
+        </CardContent>
+
+        <hr className="w-4/5 m-auto mb-4" />
+
+        <CardFooter className="flex">
+          <div className="space-y-4">
+            {["4 Team member", "4 GB Storage", "Upto 6 pages"].map(
+              (benefit: string) => (
+                <span key={benefit} className="flex">
+                  <Check className="text-green-500" />{" "}
+                  <h3 className="ml-2">{benefit}</h3>
+                </span>
+              )
+            )}
+          </div>
+        </CardFooter>
+      </Card>
+
+      {/* Service */}
+      <Card className="absolute w-[350px] -right-[10px] bottom-[35px]  drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+        <CardHeader className="space-y-1 flex md:flex-row justify-start items-start gap-4">
+          <div className="mt-1 bg-primary/20 p-1 rounded-2xl">
+            <LightBulbIcon />
+          </div>
+          <div>
+            <CardTitle>Light & dark mode</CardTitle>
+            <CardDescription className="text-md mt-2">
+              Lorem ipsum dolor sit amet consect adipisicing elit. Consectetur
+              natusm.
+            </CardDescription>
+          </div>
+        </CardHeader>
+      </Card>
+    </div>
+  );
+};
